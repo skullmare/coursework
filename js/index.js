aaf3a2800e1f33ace1137ec8e8998eaed2d06c10
skullmare <ostapchuk.ivan.2003@gmail.com>
2024-03-02 05:41:20 +0300

    update

--- js/index.js
+++ js/index.js
@@ -1,69 +1,120 @@
 let page = 1
 let countRecords = 4
 let API = '39e44460-f6f2-42a4-ad50-cb271734b64a'
-let URL = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=${API}`;
-let Data = null;
-let DataSearch = null;
-let searchMainObject = null;
-let searchName = null;
+let URLRoutes = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes?api_key=${API}`;
+let DataRoutes = null; // массив всех маршрутов
+let DataRoutesSearch = null; // массив маршрутов по результату поиска
+let searchRoutesMainObject = null; // переменная с результатом поиска по основным обьектам
+let searchRoutesName = null; // переменная с результатом поиска по названию
+let selectedRouteId = null; // айди выбранного маршрута
+let selectedRouteName = null; // название выбранного маршрута
+let DataGuides = null; // массив всех гидов
+let searchLanguage = null; // переменная с результатом поиска по языкам
+let DataGuidesSearch = null; // массив гидов по результату поиска
 
-let flagRenderOptionMainObject = false;
+
 
 // загрузка JSON из API
-function loadDATA() {
-    fetch(URL)
+function loadDATARoutes() {
+    fetch(URLRoutes)
         .then(function (response) {
             return response.json();
         }).then(function (myJSON) {
-            Data = myJSON;
+            DataRoutes = myJSON;
+            renderOptionMainObject(DataRoutes)
+        });
+}
+
+function loadDATAGuides() {
+    let URLGuides = `http://exam-2023-1-api.std-900.ist.mospolytech.ru/api/routes/${selectedRouteId}/guides?api_key=${API}`;
+    fetch(URLGuides)
+        .then(function (response) {
+            return response.json();
+        }).then(function (myJSON) {
+            DataGuides = myJSON;
+            renderOptionLanguage()
         });
 }
 
 
-
-let interval = setInterval(go, 1000)
-
+let intervalLoadDATARoutes = setInterval(goRoutes, 1000)
 
 
-function go() {
-    if (Data != null) {
+function goRoutes() {
+    if (DataRoutes != null) {
 
 
         // -----------------------------------
-        
-        if (searchName != null) {
-            DataSearch = Data.filter(function (Data) {
-                return Data.name.includes(searchName);
+
+        if (searchRoutesName != null) {
+            DataRoutesSearch = DataRoutes.filter(function (DataRoutes) {
+                return DataRoutes.name.includes(searchRoutesName);
             });
-        }else if (searchMainObject != null) {
-            DataSearch = Data.filter(function (Data) {
-                return Data.mainObject.includes(searchMainObject);
+        } else if (searchRoutesMainObject != null) {
+            DataRoutesSearch = DataRoutes.filter(function (DataRoutes) {
+                return DataRoutes.mainObject.includes(searchRoutesMainObject);
             });
-        }else {
-            DataSearch = null;
+        } else {
+            DataRoutesSearch = null;
         }
 
 
 
         // -----------------------------------
-        if (DataSearch != null) {
-            render(DataSearch)
+        if (DataRoutesSearch != null) {
+            renderRoutesGo(DataRoutesSearch)
         }
         else {
-            render(Data)
+            renderRoutesGo(DataRoutes)
         }
 
         // -----------------------------------
 
-        if (!flagRenderOptionMainObject) {
-            renderOptionMainObject(Data)
-            flagRenderOptionMainObject = true;
-        }
+
 
 
 
         // -----------------------------------
-        clearInterval(interval);
+        clearInterval(intervalLoadDATARoutes);
+    }
+}
+
+
+let intervalLoadDATAGuides = null;
+
+
+
+document.querySelector('.guidesBlock').style.display = 'none'
+
+
+
+function goGuides() {
+    if (DataGuides != null) {
+        if (searchLanguage != null) {
+            DataGuidesSearch = DataGuides.filter(function (DataGuides) {
+                return DataGuides.language.includes(searchLanguage);
+            });
+        } else {
+            DataGuidesSearch = null;
+        }
+
+
+        // -----------------------------------
+        if (DataGuidesSearch != null) {
+            renderGuidesGo(DataGuidesSearch)
+        }
+        else {
+            renderGuidesGo(DataGuides)
+        }
+
+
+
+
+
+
+
+        // -----------------------------------
+        clearInterval(intervalLoadDATAGuides);
     }
 }
 
@@ -72,18 +123,20 @@ function go() {
 
 
 
+function renderRoutesGo(DataRoutes) {
+    renderBtnPage(DataRoutes.length)
+    renderRoutes(DataRoutes, countRecords)
+}
 
-
-function render(Data) {
-    renderBtnPage(Data.length)
-    renderRoutes(Data, countRecords)
+function renderGuidesGo(DataGuides) {
+    renderGuides(DataGuides)
 }
 
 
 
 
 // заполнение таблицы
-const renderRoutes = (DATA, countRecords) => {
+const renderRoutes = (DATARoutes, countRecords) => {
     let routes = document.querySelector('.routes'); // tbody
     let template = document.querySelector('#route'); // template
     routes.innerHTML = '';
@@ -95,10 +148,14 @@ const renderRoutes = (DATA, countRecords) => {
         let mainObject = clone.querySelector('.mainObject');
         let btnLoadGuides = clone.querySelector('.btnLoadGuides')
         try {
-            name.textContent = DATA[i].name;
-            description.textContent = DATA[i].description;
-            mainObject.textContent = DATA[i].mainObject;
-            btnLoadGuides.id = DATA[i].id;
+            if (DATARoutes[i].id == selectedRouteId) {
+                let tr = clone.querySelector('tr')
+                tr.classList.add('selected-route');
+            }
+            name.textContent = DATARoutes[i].name;
+            description.textContent = DATARoutes[i].description;
+            mainObject.textContent = DATARoutes[i].mainObject;
+            btnLoadGuides.id = DATARoutes[i].id;
             routes.append(clone);
         }
         catch {
@@ -111,7 +168,28 @@ const renderRoutes = (DATA, countRecords) => {
 }
 
 
+const renderGuides = (DataGuides) => {
+    selectedRouteName = DataRoutes.find(Data => Data.id == selectedRouteId).name;
+    let titleGuides = document.querySelector('.titleGuides');
+    titleGuides.textContent = 'Доступные гиды по маршруту ' + '"' + selectedRouteName + '"';
 
+
+    let guides = document.querySelector('.guides');
+    let template = document.querySelector('#guide');
+    guides.innerHTML = '';
+    for (let i = 0; i < DataGuides.length; i++) {
+        let clone = template.content.cloneNode(true);
+        let nameGuide = clone.querySelector('.nameGuide');
+        let language = clone.querySelector('.language');
+        let workExperience = clone.querySelector('.workExperience');
+        let pricePerHour = clone.querySelector('.pricePerHour');
+        nameGuide.textContent = DataGuides[i].name;
+        language.textContent = DataGuides[i].language;
+        workExperience.textContent = DataGuides[i].workExperience;
+        pricePerHour.textContent = DataGuides[i].pricePerHour + ' ₽';
+        guides.append(clone)
+    }
+}
 
 
 // генерация кнопок для переключения страниц записей
@@ -142,16 +220,24 @@ const eventBtnPages = () => {
     for (let i = 0; i < btnPages.length; i++) {
         btnPages[i].addEventListener('click', function () {
             page = i + 1;
-            go();
+            goRoutes();
         })
     }
 }
 const eventbtnLoadGuides = () => {
     let btnLoadGuides = document.querySelectorAll('.btnLoadGuides')
+
     // -----------------------------------
     for (let i = 0; i < btnLoadGuides.length; i++) {
         btnLoadGuides[i].addEventListener('click', function () {
-            console.log(btnLoadGuides[i].id)
+            document.querySelector('.guidesBlock').style.display = 'block';
+            selectedRouteId = btnLoadGuides[i].id;
+            goRoutes();
+            searchLanguage = null;
+            DataGuidesSearch = null;
+            loadDATAGuides();
+
+            intervalLoadDATAGuides = setInterval(goGuides, 1000);
         })
     }
 
@@ -165,15 +251,15 @@ function truncate(str, max) {
 
 
 // генерация выпадающего списка основных обьектов
-const renderOptionMainObject = (DATA) => {
+const renderOptionMainObject = (DATARoutes) => {
     let selectMainObjects = document.querySelector('.selectMainObjects');
     let template = document.querySelector('#optionMainObject');
     let arrayMainObjects = []
     let splitMainObject = []
     let finalArrayMainObject = []
-    for (let i = 0; i < DATA.length; i++) {
-        arrayMainObjects.push(DATA[i].mainObject);
-    }    
+    for (let i = 0; i < DATARoutes.length; i++) {
+        arrayMainObjects.push(DATARoutes[i].mainObject);
+    }
     for (let i = 0; i < arrayMainObjects.length; i++) {
         splitMainObject = arrayMainObjects[i].split(/\-|–|—/);
         finalArrayMainObject.push(splitMainObject)
@@ -188,8 +274,35 @@ const renderOptionMainObject = (DATA) => {
     }
 }
 
+// генерация выпадающего списка доступных языков
+const renderOptionLanguage = () => {
+    let selectLanguages = document.querySelector('.selectLanguages');
+    let template = document.querySelector('#optionLanguage');
+    selectLanguages.innerHTML = ''
+    let selectedNull = document.createElement('option');
+    selectedNull.classList.add('optionLanguage')
+    selectedNull.innerText = 'Не выбрано';
+    selectLanguages.appendChild(selectedNull);
+    // -----------------------------------
+    for (let i = 0; i < DataGuides.length; i++) {
+        let clone = template.content.cloneNode(true);
+        let optionLanguage = clone.querySelector('.optionLanguage');
+        optionLanguage.textContent = DataGuides[i].language;
+        selectLanguages.append(clone);
+    }
+}
 
-
+// событие для выпадающего списка доступных языков
+const eventSelectLanguage = () => {
+    let selectLanguages = document.querySelector('.selectLanguages');
+    selectLanguages.addEventListener('change', function () {
+        searchLanguage = selectLanguages.value;
+        if (searchLanguage == 'Не выбрано') {
+            searchLanguage = null;
+        }
+        goGuides();
+    })
+}
 
 
 // событие для выпадающего списка поиска
@@ -198,30 +311,30 @@ const eventSelectMainObject = () => {
     // -----------------------------------
     selectMainObjects.addEventListener('change', function () {
         if ((selectMainObjects.value).length > 60) {
-            searchMainObject = (selectMainObjects.value).slice(0,-3); // убираем точки в конце
+            searchRoutesMainObject = (selectMainObjects.value).slice(0, -3); // убираем точки в конце
         } else {
-            searchMainObject = selectMainObjects.value;
+            searchRoutesMainObject = selectMainObjects.value;
         }
-        if (searchMainObject == 'Не выбрано') {
-            searchMainObject = null;
+        if (searchRoutesMainObject == 'Не выбрано') {
+            searchRoutesMainObject = null;
         }
         page = 1;
-        searchName = null;
-        go();
+        searchRoutesName = null;
+        goRoutes();
     })
 }
 
 let inputSearch = document.querySelector('.inputSearch');
 inputSearch.addEventListener('input', function () {
-    searchName = inputSearch.value;   
-    if (searchName == '') {
-        searchName = null;
-    } 
+    searchRoutesName = inputSearch.value;
+    if (searchRoutesName == '') {
+        searchRoutesName = null;
+    }
     page = 1;
-    searchMainObject = null;
-    go();
+    searchRoutesMainObject = null;
+    goRoutes();
 })
 
-
+eventSelectLanguage();
 eventSelectMainObject()
-window.onload = loadDATA()
+window.onload = loadDATARoutes()
